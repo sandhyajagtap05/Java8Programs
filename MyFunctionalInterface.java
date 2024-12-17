bbedf70f2ee71f2263580502345af7bcd2e89027
sandhya Jagtap <sandhyajagtap05022000@gmail.com>
2024-12-17 14:34:46 +0530

    methodReferenceWithParametherisedConstructo

---
+++ MyFunctionalInterface.java
@@ -0,0 +1,8 @@
+package org.example.methodReferenceWithParametherisedConstructor;
+
+@FunctionalInterface
+public interface MyFunctionalInterface {
+
+        Student create(int id,String name,String address);
+
+}
